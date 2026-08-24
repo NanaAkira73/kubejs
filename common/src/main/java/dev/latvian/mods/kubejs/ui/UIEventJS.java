f6aee302f875f7b1d8b15f3873119a8e33105db3
一般路过阿七 <126596611+NanaAkira73@users.noreply.github.com>
2026-08-24 22:08:38 +0800

    Fix: add throws EventExit to UIEventJS.replace()

--- common/src/main/java/dev/latvian/mods/kubejs/ui/UIEventJS.java
+++ common/src/main/java/dev/latvian/mods/kubejs/ui/UIEventJS.java
@@ -1,5 +1,6 @@
 package dev.latvian.mods.kubejs.ui;
 
+import dev.latvian.mods.kubejs.event.EventExit;
 import dev.latvian.mods.kubejs.event.EventJS;
 import dev.latvian.mods.kubejs.ui.widget.UI;
 
@@ -9,7 +10,7 @@ public class UIEventJS extends EventJS {
 	public Consumer<UI> consumer;
 	public int forcedScale = -1;
 
-	public void replace(Consumer<UI> consumer) {
+	public void replace(Consumer<UI> consumer) throws EventExit {
 		this.consumer = consumer;
 		cancel();
 	}
